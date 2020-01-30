8fd9cde05768e7bde52b2c3a555f651507fd1ce0
Danica Valdez <danicaevaldez@gmail.com>
2020-01-29 19:32:30 -0900

    MVP Completed

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const AccountRouter = require('./routers/accounts-router');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', AccountRouter);
+
+server.get("/", (req, res) => {
+  res.send('<h1>Hello World</h1>')
+});
 
 module.exports = server;
